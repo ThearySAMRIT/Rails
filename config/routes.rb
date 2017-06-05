d74a3d9adfe69f7ab3db16407a9e31068a9f4983
ThearySTR <somrithsotheary@gmail.com>
2017-06-05 14:33:19 +0700

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/help'
-  root 'static_pages#home'
+  get "static_pages/about"
+  get "static_pages/contact"
+  get "static_pages/help"
+  root "static_pages#home"
 end
